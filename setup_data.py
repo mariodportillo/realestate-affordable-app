2dd3b33197a43283f313c46517c7a42e4c7a6160
mariodportillo <mariodportillo@gmail.com>
2025-05-26 18:42:34 -0700

    gdown

--- setup_data.py
+++ setup_data.py
@@ -2,6 +2,10 @@ import gdown
 import os
 
 os.makedirs("data", exist_ok=True)
-file_id = "1fGZGWALwTI0Auxzxs5lubuQqlBppxJBP"  # Replace with your actual file ID
+file_id = "1fGZGWALwTI0Auxzxs5lubuQqlBppxJBP"  # Your actual file ID
 output = "data/EVERY_LISTING.gz"
-gdown.download("https://drive.google.com/uc?id={file_id}", output, quiet=False)
+
+# Use an f-string to interpolate file_id
+url = f"https://drive.google.com/uc?id={file_id}"
+
+gdown.download(url, output, quiet=False)
